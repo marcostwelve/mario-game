b02820f54bc89a7dd550d36d688681cd3a875603
marcostwelve <marcostwelve>
2023-10-21 00:58:54 -0300

    Mario Game v2

--- js/script.js
+++ js/script.js
@@ -1,74 +1,68 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clouds = document.querySelector('.count');
-const score = document.querySelector('.score');
-const currentCount = document.querySelector('.count');
-const start = document.querySelector('.start');
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
+const start = document.querySelector(".start");
 const reload = document.querySelector(".reload");
-let alreadyJump = false;
-let currentScore = 0;
+const score = document.querySelector(".score");
+let count = 0;
 
-start.addEventListener('click', (e) => {
-  e.preventDefault();
-  mario.classList.remove("hidden");
-  pipe.classList.remove("hidden");
-  clouds.classList.remove("hidden");
-  score.classList.remove("hidden");
-  start.classList.add("hidden");
-  document.addEventListener('keydown', (e) => {
-    if(e.code === "ArrowUp" || e.code === "Space") {
-      jump();
-    }
-  });
-
-  const watch = setInterval(() => {
-    currentScore++;
-    currentCount.textContent = currentScore;
-    }, 1000);
-
-  const loop = setInterval(() => {
-  
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-  
-    if(pipePosition <= 75  && pipePosition > 0 && marioPosition < 80) {
-      reload.classList.remove("hidden");
-      pipe.style.animation = "none";
-      pipe.style.left = `${pipePosition}px`;
-  
-      mario.style.animation = "none";
-      mario.style.bottom = `${marioPosition}px`;
-  
-      mario.src = "./assets/game-over.png";
-      mario.style.width = "40px";
-      mario.style.marginLeft = "50px";
-      clearInterval(watch);
-      clearInterval(loop);
-    }
-
-  if(currentScore > 10) {
-    pipe.style.animationDuration = "1s";
-  }
-  
-  }, 10);
-});
-
-reload.addEventListener('click', (e) => {
-  bestScore = currentScore;
-  currentScore = 0;
-  window.location.reload();
-
-})
 
 const jump = () => {
-  if(!mario.classList.contains('jump')) {
-    mario.classList.add('jump');
-    alreadyJump = true;
+  if(!mario.classList.contains("jump")) {
+    mario.classList.add("jump");
+    
   }
 
   setTimeout(() => {
-    mario.classList.remove('jump');
-    alreadyJump = false;
+    mario.classList.remove("jump");
   }, 500);
-  
+
+  count++;
 }
+
+
+
+const loop = setInterval(() => {
+  
+  const pipePosition = pipe.offsetLeft;
+  let marioBottom = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+  if(pipePosition <= 65 && pipePosition > 0 && marioBottom < 90) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioBottom}`;
+
+    mario.src = "./assets/game-over.png";
+    mario.style.width = "3em";
+    mario.style.marginLeft = "50px";
+
+    alert(`Game over! Seu score foi ${count}`);
+    count = 0;
+
+    clearInterval(loop);
+
+    reload.classList.remove("hidden");
+  }
+
+  score.innerHTML = `Score ${count}`;
+
+}, 10);
+
+
+start.addEventListener("click", () => {
+  start.classList.add("hidden");
+  mario.classList.remove("hidden");
+  pipe.classList.remove("hidden");
+  score.classList.remove("hidden");
+  clouds.classList.remove("hidden");
+});
+
+reload.addEventListener("click", () => {
+  reload.classList.add("hidden");
+  window.location.reload();
+});
+
+
+document.addEventListener("keydown", jump);
